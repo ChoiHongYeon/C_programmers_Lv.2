7efe6b282c95ac7784a477d161a8b0c9fc7b1ae2
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-07-17 22:11:27 +0900

    first commit

---
+++ 20240717-1.c
@@ -0,0 +1,47 @@
+﻿// 마법의 엘리베이터 (https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=c)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int storey) {
+
+    int answer = 0;
+    int n = 1;
+
+    while (storey > 0) {
+        int i = (storey % (n * 10)) / n;
+        switch (i) {
+        case 0:
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            storey -= i * n;
+            answer += i;
+            break;
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            storey += 10 - i * n;
+            answer += 10 - i;
+            break;
+        }
+        n *= 10;
+    }
+
+    return answer;
+
+}
+
+void main()
+{
+    
+    int result1 = solution(16);
+    int result2 = solution(2554);
+    printf("%d %d", result1, result2);
+
+}
