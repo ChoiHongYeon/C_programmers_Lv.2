ab21222c108a544742ebc0e49dc7d907d1c58572
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-10-07 20:38:00 +0900

    first commit

---
+++ 20241007-1.c
@@ -0,0 +1,43 @@
+﻿// 올바른 괄호 (https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=c)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool solution(const char* s) {
+
+    bool answer = true;
+    int n = 0;
+
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] == '(') {
+            if (n < 0) {
+                answer = false;
+                break;
+            }
+            n++;
+        }
+        else {
+            n--;
+        }
+    }
+
+    if (n != 0)
+        answer = false;
+
+    return answer;
+
+}
+
+void main()
+{
+
+    const char* s1 = "()()";
+    const char* s2 = "(())()";
+    const char* s3 = ")()(";
+    const char* s4 = "(()(";
+
+    printf("%d %d %d %d", solution(s1), solution(s2), solution(s3), solution(s4));
+
+}
