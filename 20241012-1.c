9eebf95729470e47ba72b2aacc936073ae39651b
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-10-12 16:36:43 +0900

    first commit

---
+++ 20241012-1.c
@@ -0,0 +1,28 @@
+﻿// 피보나치 수 (https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=c)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+
+    int a = 1;
+    int b = 1;
+    int bb = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        a = (b + bb) % 1234567;
+        bb = b;
+        b = a;
+    }
+
+    return a;
+
+}
+
+void main()
+{
+
+    printf("%d %d", solution(3), solution(5));
+
+}
